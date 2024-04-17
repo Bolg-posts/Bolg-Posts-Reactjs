0cfe5f746e42ba059fb2ed6a38958b2b3f11c0e0
yasmeen10 <yasmeenwaleed844@gmail.com>
2024-04-17 13:14:23 +0200

    homeUpdate

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,25 @@ import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    async function fetchPostsData() {
+      const { data } = await axios.get("http://localhost:3000/posts");
+      console.log(data);
+      setPosts(data);
+    }
+    fetchPostsData();
+  }, []);
   return (
     <>
       <BrowserRouter>
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home />}></Route>
+          <Route path="/" element={<Home posts={posts} />}></Route>
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
         </Routes>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,19 @@
 import React from "react";
 
-export default function Home() {
-  return <div>Home</div>;
+export default function Home(props) {
+  const { posts } = props;
+  console.log(posts);
+  return (
+    <div>
+      <div>
+        {posts.map((post) => (
+          <div key={post._id}>
+            <h1>{post.title}</h1>
+            <p>{post.description}</p>
+            <img src={post.image} alt="image" />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
